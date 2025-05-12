53d4606c22837abbf89009e2fc6bc0d88c24f589
scraproace <scraproace@gmail.com>
2025-05-12 19:52:58 +0900

    fonts change

--- module/const/constants.py
+++ module/const/constants.py
@@ -1,4 +1,4 @@
 CLOSING_DAY = 15  # 締め日
 GOAL_AMOUNT = 100_000  # 目標金額
 LIMIT_AMOUNT = 1_030_000  # 限度額
-PIE_FONTPATH = 'C:/Windows/Fonts/msgothic.ttc'  # 円グラフのフォントパス
+PIE_FONTPATH = './fonts/msgothic.ttc'  # 円グラフのフォントパス
